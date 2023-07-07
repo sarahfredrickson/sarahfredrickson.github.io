43a673b346c21f1bac3c2ece987752250a972760
sarahsf <sarahsf@cs.washington.edu>
2023-07-06 20:28:37 -0700

    template loaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import About from './component/About'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import Home from './component/Home'
+import Portfolio from './component/Portfolio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="page">
+        <div id="wrap">
+          <Header />
+          <div id="main">
+            <Home />
+            <About />
+            <Portfolio />
+          </div>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
